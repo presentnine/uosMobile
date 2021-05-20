37937b4b877eb104ed81c069d5981512d6e7d6a0
GoonLee <auteur.kor@gmail.com>
2021-05-20 18:09:06 +0900

    Initial commit

---
+++ app/src/main/java/com/uosmobile/team1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.uosmobile.team1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
